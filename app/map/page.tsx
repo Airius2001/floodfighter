0dd9ca1e6a143db6ba80009ddeea49b91b30f55e
Airius2001 <zxuu0075@student.monash.edu>
2025-09-04 18:26:20 +1000

    complete the map visual functions

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,14 +1,19 @@
-"use client";
+'use client';
 
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
+import React from 'react';
 
-const WaterMap = dynamic(() => import("../map/watermap"), { ssr: false });
+const CombinedMap = dynamic(() => import('./combined').then((mod) => mod.default), {
+  ssr: false,
+});
 
-export default function Home() {
+export default function MapPage() {
   return (
-    <div>
-      <h1>Australia reservoir map distribution</h1>
-      <WaterMap />
+    <div style={{ padding: '20px' }}>
+      <h1>Australia Flood information Map</h1>
+      <div style={{ marginTop: '20px', height: '90vh' }}>
+        <CombinedMap />
+      </div>
     </div>
   );
 }
